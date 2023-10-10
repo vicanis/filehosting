d92967321604bd089767ca465187af060b9c9a01
Victor Anisimov <admin@cloud.fbuzz.pro>
2023-10-11 01:59:23 +0400

    feat: add .env support

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/vicanis/filehosting/server"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error while reading .env: %s", err)
+	}
+
 	log.Fatal(server.Start())
 }
